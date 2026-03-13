692c4d3a616639f91fd50c32c23080ae1fd50431
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-14 00:52:00 +0300

    ленд по кнопке
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

---
+++ lobby/lobby.view.css.ts
@@ -0,0 +1,12 @@
+namespace $.$$ {
+	$mol_style_define($bog_blitz_lobby, {
+		Qr: {
+			width: '200px',
+			height: '200px',
+			border: {
+				radius: $mol_gap.round,
+			},
+			align: { self: 'center' },
+		},
+	})
+}
--- lobby/lobby.view.ts
+++ lobby/lobby.view.ts
@@ -1,3 +1,41 @@
 namespace $.$$ {
-	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {}
+	export class $bog_blitz_lobbyLand extends $giper_baza_entity.with({
+		Name: $giper_baza_atom_text,
+	}) {}
+	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {
+		@$mol_mem
+		land_link(next?: string | null) {
+			return this.$.$mol_state_arg.value('land', next) ?? ''
+		}
+
+		@$mol_mem
+		land() {
+			const link = this.land_link()
+			if (!link) return null
+			return this.$.$giper_baza_glob.Land(new $giper_baza_link(link))
+		}
+
+		@$mol_action
+		land_create() {
+			const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('late')]])
+			this.land_link(land.link().str)
+			return land
+		}
+
+		@$mol_mem
+		invite_link() {
+			const link = this.land_link()
+			if (!link) return ''
+			const url = new URL(this.$.$mol_dom_context.location.href)
+			url.searchParams.set('land', link)
+			return url.toString()
+		}
+
+		@$mol_mem
+		qr_uri() {
+			const invite = this.invite_link()
+			if (!invite) return ''
+			return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(invite)}`
+		}
+	}
 }
