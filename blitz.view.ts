fb6502094377196f098ba452e76959f3feb63f67
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-14 00:09:42 +0300

    переключение страниц
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- blitz.view.ts
+++ blitz.view.ts
@@ -1,3 +1,12 @@
 namespace $.$$ {
-	export class $bog_blitz extends $.$bog_blitz {}
+	export class $bog_blitz extends $.$bog_blitz {
+		screen_body() {
+			const key = this.screen()
+			const label = this.Navbar().options()[key] ?? key
+			return [label + ' screen']
+		}
+		screen(next?: string) {
+			return this.$.$mol_state_arg.value('screen', next) ?? 'lobby'
+		}
+	}
 }
