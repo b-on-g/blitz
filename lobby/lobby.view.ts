d6851606bddac5e772635ca70930c141871e86e8
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-15 12:19:38 +0300

    один метод
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- lobby/lobby.view.ts
+++ lobby/lobby.view.ts
@@ -1,17 +1,13 @@
 namespace $.$$ {
 	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {
 		@$mol_mem
-		land_ensure() {
-			const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
-			this.$.$mol_state_arg.value('land', land.link().str)
-		}
-
-		@$mol_mem
 		qr_uri() {
 			let link = this.$.$mol_state_arg.value('land') ?? ''
+
 			if (!link) {
-				this.land_ensure()
-				return ''
+				const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
+				link = land.link().str
+				this.$.$mol_state_arg.value('land', link)
 			}
 
 			const invite = this.$.$mol_state_arg.link({ land: link })
