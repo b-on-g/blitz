79a256062688be09be4a459d7a23cb3c261a958c
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-15 14:37:03 +0300

    clean
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- lobby/lobby.view.ts
+++ lobby/lobby.view.ts
@@ -1,20 +1,19 @@
 namespace $.$$ {
 	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {
-		// @$mol_mem
-		// qr_uri() {
-		// 	let link = this.$.$mol_state_arg.value('land') ?? ''
-		// 	if (!link) {
-		// 		const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
-		// 		link = land.link().str
-		// 		this.$.$mol_state_arg.value('land', link)
-		// 	}
-		// 	const invite = this.$.$mol_state_arg.link({ land: link })
-		// 	return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(invite)}`
-		// }
-		div_text() {
+		@$mol_mem
+		land_create() {
 			const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
-			return land.link().str
-			// return 'sdfsd'
+			this.$.$mol_state_arg.value('land', land.link().str)
+		}
+
+		@$mol_mem
+		qr_uri() {
+			let link = this.$.$mol_state_arg.value('land') ?? ''
+			if (!link) {
+				this.land_create()
+			}
+			const invite = this.$.$mol_state_arg.link({ land: link })
+			return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(invite)}`
 		}
 	}
 }
