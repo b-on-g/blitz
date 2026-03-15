51fd1074541a12cce689c25cdb3a4ce6703a2ac8
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-15 16:18:30 +0300

    qr!
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- lobby/lobby.view.css.ts
+++ lobby/lobby.view.css.ts
@@ -1,8 +1,6 @@
 namespace $.$$ {
 	$mol_style_define($bog_blitz_lobby, {
 		Qr: {
-			width: '200px',
-			height: '200px',
 			border: {
 				radius: $mol_gap.round,
 			},
--- lobby/lobby.view.ts
+++ lobby/lobby.view.ts
@@ -1,14 +1,13 @@
 namespace $.$$ {
 	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {
 		@$mol_mem
-		qr_uri() {
+		qr_data() {
 			let link = this.$.$mol_state_arg.value('land') ?? ''
 			if (!link) {
 				const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
 				this.$.$mol_state_arg.value('land', land.link().str)
 			}
-			const invite = this.$.$mol_state_arg.link({ land: link })
-			return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(invite)}`
+			return this.$.$mol_state_arg.link({ land: link })
 		}
 	}
 }
