f870fa1fbe1e1cdbdd38761681f47f3dd1136c16
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-15 12:18:42 +0300

    не победил
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- game/game.view.ts
+++ game/game.view.ts
@@ -1,8 +1,3 @@
 namespace $.$$ {
-	export class $bog_blitz_game extends $.$bog_blitz_game {
-		auto() {
-			this.$.$mol_state_arg.value('land', null)
-			super.auto()
-		}
-	}
+	export class $bog_blitz_game extends $.$bog_blitz_game {}
 }
--- lobby/lobby.view.ts
+++ lobby/lobby.view.ts
@@ -1,19 +1,21 @@
 namespace $.$$ {
 	export class $bog_blitz_lobby extends $.$bog_blitz_lobby {
+		@$mol_mem
+		land_ensure() {
+			const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
+			this.$.$mol_state_arg.value('land', land.link().str)
+		}
 
 		@$mol_mem
 		qr_uri() {
 			let link = this.$.$mol_state_arg.value('land') ?? ''
-
 			if (!link) {
-				const land = this.$.$giper_baza_glob.land_grab([[null, $giper_baza_rank_post('slow')]])
-				link = land.link().str
-				this.$.$mol_state_arg.value('land', link)
+				this.land_ensure()
+				return ''
 			}
 
 			const invite = this.$.$mol_state_arg.link({ land: link })
 			return `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(invite)}`
 		}
-
 	}
 }
