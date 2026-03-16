7820ea53881963f431da273b9a08dfa4790113df
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-16 23:00:22 +0300

    ыва
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- lobby/waiting/waiting.view.css.ts
+++ lobby/waiting/waiting.view.css.ts
@@ -1,3 +1,16 @@
 namespace $.$$ {
-	$mol_style_define($bog_blitz_lobby_waiting, {})
+	$mol_style_define($bog_blitz_lobby_waiting, {
+		Waiting_message: {
+			align: { self: 'center' },
+			font: { size: '1.5rem', weight: 500 },
+			opacity: 0.6,
+			padding: { bottom: '0.5rem', top: '1rem' },
+		},
+		Counter: {
+			align: { self: 'center' },
+			font: { size: '1.25rem', weight: 500 },
+			opacity: 0.7,
+			padding: { bottom: '1.5rem', top: '1rem' },
+		},
+	})
 }
