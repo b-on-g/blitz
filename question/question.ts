68a0642c1116d250c077bc2c01662150f378c1bf
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-17 13:06:52 +0300

    мульти чойс и чойс одинаково работают
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- question/question.ts
+++ question/question.ts
@@ -3,7 +3,6 @@ namespace $.$$ {
 		Text: $giper_baza_atom_text,
 		Type: $giper_baza_atom_text,
 		Options: $giper_baza_list_link_to(() => $bog_blitz_question_option),
-		Correct_option: $giper_baza_atom_real,
 		Correct_text: $giper_baza_atom_text,
 		Image: $giper_baza_atom_link_to(() => $giper_baza_file),
 	}) {}
