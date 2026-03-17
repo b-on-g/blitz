cee3a999cd19d3d6176192c5fe883ca476a274e4
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-17 14:55:22 +0300

    порядок
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- admin/question/question.view.ts
+++ admin/question/question.view.ts
@@ -2,7 +2,8 @@ namespace $.$$ {
 	export class $bog_blitz_admin_question extends $.$bog_blitz_admin_question {
 		@$mol_mem
 		question_rows() {
-			const rows = [this.Header(), this.Text_input()]
+			const rows = [this.Text_input(), this.Header()]
+
 			if (this.question_type() === 'text_input') {
 				rows.push(this.Correct_text_input())
 			} else {
