13a4a16fd439adf5a58420c4dc77bda85934a4bb
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-23 16:08:06 +0300

    выравнивание
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- blitz.view.css.ts
+++ blitz.view.css.ts
@@ -22,12 +22,18 @@ namespace $.$$ {
 			justify: {
 				content: 'flex-end',
 			},
+			align: {
+				items: 'center',
+			},
 		},
 
 		Version: {
 			opacity: 0.3,
 			font: {
 				size: '.75rem',
+			},
+			margin: {
+				right: '.5rem',
 			},
 		},
 
