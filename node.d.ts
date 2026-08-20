8ebf2e3f606ed6ead463ea23421f91ef4845edb8
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-08-20 07:32:10 +0000

    Deploying to gh-pages from @ b-on-g/blitz@17cdde45326006c3c161c56dc400025189959f30 🚀

--- node.d.ts
+++ node.d.ts
@@ -3957,6 +3957,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $giper_baza_log(this: $): boolean;
+}
+
+declare namespace $ {
     enum $giper_baza_slot_kind {
         /** Free Unit Slot */
         free = 0,
@@ -4558,10 +4562,6 @@ declare namespace $ {
         destructor(): void;
         send(data: Data): void;
     }
-}
-
-declare namespace $ {
-    function $giper_baza_log(this: $): boolean;
 }
 
 declare namespace $ {
