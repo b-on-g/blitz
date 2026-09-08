4f20cdd672f9f46ed59965bcd59dcf3cfb02c573
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-09-08 11:49:19 +0000

    Deploying to gh-pages from @ b-on-g/blitz@17cdde45326006c3c161c56dc400025189959f30 🚀

--- blog/node.test.js
+++ blog/node.test.js
@@ -8365,6 +8365,7 @@ var $;
     class $mol_rest_port extends $mol_object {
         send_code(code) { }
         send_type(mime) { }
+        send_name(name) { }
         origin() { return 'unknown'; }
         address() { return 'unknown'; }
         send_data(data) {
@@ -14946,7 +14947,7 @@ var $;
                 const tx = this.sides[0].open('read_only');
                 const data = tx.read();
                 tx.destructor();
-                this.pool().acquire(data.byteLength);
+                this.pool().acquire(Math.ceil(data.byteLength / 8) * 8);
                 return data;
             }
             catch (error) {
@@ -16788,6 +16789,15 @@ var $;
                 return;
             this.output.setHeader('content-type', mime);
         }
+        send_name(name) {
+            if (this.output.writableEnded)
+                return;
+            if (this.output.getHeader('content-disposition'))
+                return;
+            const utf8 = encodeURIComponent(name)
+                .replace(/['()*]/g, char => '%' + char.charCodeAt(0).toString(16).toUpperCase());
+            this.output.setHeader('content-disposition', `inline; filename*=UTF-8''${utf8}`);
+        }
         send_bin(data) {
             if (this.output.writableEnded)
                 return;
@@ -16801,6 +16811,9 @@ var $;
     __decorate([
         $mol_action
     ], $mol_rest_port_http.prototype, "send_type", null);
+    __decorate([
+        $mol_action
+    ], $mol_rest_port_http.prototype, "send_name", null);
     __decorate([
         $mol_action
     ], $mol_rest_port_http.prototype, "send_bin", null);
